4075570793d1de88a5b9e49b7dfb4082fa5c9c21
Abdelrhman-Mnajaa <abdelrhmanmnajaa@gmail.com>
2023-09-28 11:06:05 +0300

    update

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { Fragment } from 'react';
+import  SignupForm  from './Components/Task1';
+import  Calculator  from './Components/Task2';
+import  Calculating  from './Components/Task3';
+
+
+
+// function App() {
+//     return (
+//        
+//     );
+//   }
+  
+  // export default App;
+
+
+
+  // function App() {
+  //   return (
+  //       
+  //   );
+  // }
+
+  // export default App;
+  
+
+
+  function App() {
+    return (
+          <>
+          <Calculating/>
+           <Calculator/>
+           <SignupForm/>
+           </>
+    );
+   }
+  
+  export default App;
